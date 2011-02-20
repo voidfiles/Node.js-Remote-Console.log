fa2c783ef0f743587028fe06522058dbe5af6d9b
Tim Schaub <tim.schaub@gmail.com>
2011-02-20 15:56:05 +0100

    This document write breaks things on iOS browsers.  Doesn't appear to serve a purpose, so I'm removing it.

--- remote_console.js
+++ remote_console.js
@@ -60,7 +60,7 @@ function returnDebugJS(ns){
     return '(function(){var log=function(){log.history=log.history||[];log.history.push(arguments);' + 
     'var message=Array.prototype.slice.call(arguments),args=JSON.stringify(message), ' + 
     'img=document.createElement("img"),url="http://'+host+':'+port+'/?console="+escape(args);' + 
-    'document.write(message);img.src=url;img.style.display="none";document.body.appendChild(img);' + 
+    'img.src=url;img.style.display="none";document.body.appendChild(img);' + 
     'if(this.console){console.log(Array.prototype.slice.call(arguments))}};catchRemote=function' +
     '(func){try{func();}catch(e){log({"error":e});}};' + ns + '.log=log;})()';
     
